84d97c66a703b2e9ee825b7313b927a790c1ea99
FannyOlivia <fannyberstad@outlook.com>
2021-07-07 15:56:47 +0200

    Add proper team members

--- team_list.js
+++ team_list.js
@@ -1,9 +1,18 @@
 document.getElementById("motivation").innerHTML = "Sup world";
 var index = 0;
 
-const teamList = [{ name: "Vic", study: "Biotechnology - Systems Biology", responsibilities: "Team Captain", motivation: "power", imgUrl: "Pictures/victoria.jpg" }, {
-    name: "Andrea", study: "Biotechnology", responsibilities: "Sponsors", motivation: "money", imgUrl: "Pictures/andrea.jpg"
-}]
+const teamList = [{ name: "Adrian Thorsplass", study: "Msc. in Systems Biology, 4th Year", responsibilities: "Heme-approach, Lab on a Chip", motivation: "To build skills and experience with working as a team, while at the same time being part of something bigger that contributes to the world.", imgUrl: "Pictures/adrian.jpg" }, {
+    name: "Andrea Stallvik", study: "Msc. in Systems Biology, 3rd Year", responsibilities: "Sponsors, SqR-approach", motivation: "Putting theory into practice by using synthetic biology to solve real-world problems, and meeting people around the world with a similar passion.", imgUrl: "Pictures/andrea.jpg"
+},
+{ name: "Anine H. Olafsen", study: "Msc. in Systems Biology, 4th Year", responsibilities: "Multimedia, SqR-approach", motivation: "I look forward to use my theoretical knowledge in innovative and multidisciplinary teamwork that has a practical application. I want to gain experience with working independently and to further develop my technical skills.", imgUrl: "Pictures/anine.jpg" }
+    , { name: "Eline Wålen Østerhaug", study: "Msc. in Molecular Biology, 4th Year", responsibilities: "Heme-approach, Social Media", motivation: "To gain valuable experiences, develop new skills and put theory into practice while being part of a great team of dedicated students!", imgUrl: "Pictures/eline.jpg" },
+{ name: "Elise Damlien", study: "Msc. in Molecular Biology, 4th Year", responsibilities: "Heme-approach", motivation: "power", imgUrl: "Pictures/elise.jpg" },
+{ name: "Fanny Olivia Johannessen Berstad", study: "Msc. in Industrial Mathematics, 3rd Year", responsibilities: "Wiki, multimedia, Lab on a Chip", motivation: "Be a part of the green revolution, contribute to sustainable food production, challenge myself to use theoretical knowlegde for more practical applications.", imgUrl: "Pictures/fanny.jpg" },
+{ name: "Kristine Lippestad", study: "Msc. in Systems Biology, 4th Year", responsibilities: "SqR-approach, Social Media", motivation: "Work with a multidisciplinary team, build friendships and gain new experiences while putting my theoretical knowledge into practice.", imgUrl: "Pictures/kristine.jpg" },
+{ name: "Leik Lima-Eriksen", study: "Msc. in Signal Processing and Communication, 5th Year", responsibilities: "Wiki, Multimedia, Lab on a Chip", motivation: "Contribute to technology promoting sustainability and animal welfare, use signal processing to solve real-world problems, and gain experience from working in a multidisciplinary team", imgUrl: "Pictures/leik.jpg" },
+{ name: "Martin Eide Lien", study: "Msc. in Systems Biology, 5th Year", responsibilities: "SqR-approach, Multimedia, Lab on a Chip", motivation: "Beside the fact that iGEM is a unique opportunity to apply theory from molecular biology and create something completely new that has a real world application via synthetic biology, I also look forward to working with friends over the summer.", imgUrl: "Pictures/martin.jpg" },
+{ name: "Tina Dahlgren", study: "Msc. in Biotechnology, 4th Year", responsibilities: "SqR-approach, Social Media", motivation: "Igem is a great opportunity to gain experience in several fields while building new friendships!", imgUrl: "Pictures/tina.jpg" },
+{ name: "Victoria Nilsen Gjøvaag", study: "Msc. in Systems Biology, 4th Year", responsibilities: "Team Captain, Social Media", motivation: "To learn and develop new skills, build freiendships and contribute to a more sustainable world.", imgUrl: "Pictures/victoria.jpg" }];
 
 const updateFields = (newIndex) => {
     console.log({ newIndex })
