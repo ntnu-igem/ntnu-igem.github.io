eef5888add5142fe3cf024850bfc49e956fb5a1d
FannyOlivia <fannyberstad@outlook.com>
2021-07-19 15:09:35 +0200

    Animate dropdown menu

--- navbar.js
+++ navbar.js
@@ -1,4 +1,12 @@
+"use strict";
+
 document.getElementById("navbar_menu").onclick = function () {
     this.classList.toggle("navbar_menu_opened");
 
+}
+const elems = document.getElementsByClassName("navbar_button")
+for (const elem of elems) {
+    elem.onclick = function () {
+        elem.classList.toggle("navbar_button_active");
+    };
 }
